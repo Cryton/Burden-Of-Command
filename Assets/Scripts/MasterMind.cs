bc0db6a58993bec6c9e10ad71ad57e3fab748f1e
Joseph Jeanty <j2jeanty@gmail.com>
2014-04-14 21:15:47 -0400

    title stuff

--- Assets/Scripts/MasterMind.cs
+++ Assets/Scripts/MasterMind.cs
@@ -3,51 +3,55 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class MasterMind : MonoBehaviour {
-    public GameObject soldier;
+    
     public GameObject spawn;
-    public List<GameObject> Waypoints = new List<GameObject>(); 
-     List<ArrayList> camps ;
-    List <GameObject> peeps = new List<GameObject>();
-    public float timer;
+    //A List of prefab of Troops
+    public List<GameObject> TroopsToSpawn = new List<GameObject>();
+    //A list of camps you can walk to
+    public List<GameObject> Camps = new List<GameObject>();
+
+    List<GameObject> peeps = new List<GameObject>(100);
+    float timer;
+    public float spawntimer;
     int tracker = 0;
-    GameObject temp;
+    //GameObject temp;
+    
      
 	// Use this for initialization
 	void Start () {
-        camps = new List<ArrayList>(Waypoints.Count);
+       
+        //camps = new List<ArrayList>(Waypoints.Count);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         timer += Time.deltaTime;
-
-        if (timer >= 10)
+        
+        if (timer >= spawntimer)
         {
-            
-           temp = (Instantiate(soldier, spawn.transform.position,spawn.transform.rotation)) as GameObject;
-            
-           timer = 0;
-           if (Waypoints[tracker] = null)
-           {
-               tracker = 0;
-           }
-           MoveSoldier(tracker, temp);
-           tracker++;
+           CreateSoldier();
+          
         }
 
        
 	
 	}
-    //Add the soldier to a camp
-    void MoveSoldier(int spot, GameObject gObject)
-    {
-        //tracker = spot;
+
     
-        gObject.name = spot.ToString();
-        peeps.Add(gObject);
-        
-            
-        
+    //Create and Add the soldier to a camp
+    void CreateSoldier()
+    {
+        if (peeps.Count < peeps.Capacity)
+        {
+            GameObject temp;
+            int ran_sp = Random.Range(0, TroopsToSpawn.Count);
+            int ran_camp = Random.Range(0, Camps.Count);
+            temp = (Instantiate(TroopsToSpawn[ran_sp], spawn.transform.position, spawn.transform.rotation)) as GameObject;
+            temp.gameObject.tag = "Enemy";
+            temp.SendMessage("Move", Camps[ran_camp].transform.position);
+            timer = 0;
+        }
+           
 
     }
     //Ask for soldiers
@@ -55,7 +59,7 @@ public class MasterMind : MonoBehaviour {
     {
         GameObject[] player;
         player = GameObject.FindGameObjectsWithTag("PlayerUnit");
-        Vector3 adv= new Vector3();
+        Vector3 adv = new Vector3();
         int total = 0;
         float dist = 0;
         foreach (GameObject p in player)
@@ -66,33 +70,51 @@ public class MasterMind : MonoBehaviour {
         adv.x = adv.x / total;
         adv.y = adv.y / total;
         adv.z = adv.z / total;
-        dist = Vector3.Distance(Waypoints[0].transform.position, adv);
-        int follow=0, pos_w=0;
-        foreach (GameObject w in Waypoints)
+        //dist = the first Waypoint
+        dist = Vector3.Distance(Camps[0].transform.position, adv);
+        int follow = 0, pos_w = 0;
+        foreach (GameObject w in Camps)
         {
-            if (dist >= (Vector3.Distance( w.transform.position,adv)))
+            if (dist >= (Vector3.Distance(w.transform.position, adv)))
             {
-                dist = (Vector3.Distance( w.transform.position, adv));
+                dist = (Vector3.Distance(w.transform.position, adv));
                 pos_w = follow;
             }
             follow++;
         }
+        if (dist < 40f)
+        {
+            MoveCamps(pos_w, Camps[pos_w].transform.position);
+        }
+        else
+        {
+            MoveCamps(tracker, Camps[tracker].transform.position);
 
-        MoveCamps(pos_w);
+        }
 
 
     }
 
-    //Moves current enemys to a new camp
-    void MoveCamps(int campnumber)
+    //Moves current enemys to a camp
+    void MoveCamps(int campnumber, Vector3 point)
     {
-        if (Waypoints[campnumber + 1] != null)
+        GameObject[] GetEnemy;
+        GetEnemy = GameObject.FindGameObjectsWithTag("Enemy");
+        //player = GameObject.FindGameObjectsWithTag((campnumber - 1).ToString());
+        foreach (GameObject g in GetEnemy)
+        {
+            if (g != null)
+            {
+                g.SendMessage("Move", point);
+            }
+        }
+        /*if (Waypoints[campnumber + 1] != null)
         {
             foreach (GameObject s in peeps)
             {
-                if (s.tag == (campnumber + 1).ToString())
+                if (s.name == (campnumber + 1).ToString())
                 {
-                    s.tag = campnumber.ToString();
+                    s.name = campnumber.ToString();
                 }
             }
         }
@@ -101,15 +123,42 @@ public class MasterMind : MonoBehaviour {
         {
             foreach (GameObject s in peeps)
             {
-                if (s.tag == (campnumber - 1).ToString())
+                if (s.name == (campnumber - 1).ToString())
                 {
-                    s.tag = campnumber.ToString();
+                    s.name = campnumber.ToString();
                 }
             }
-        }
+        }*/
 
     }
-    
+
 
 
 }
+
+
+public class SP_Enemy
+    {
+
+        public int camp_at
+        {
+            get;
+
+            set;
+        }
+
+        public int tag_as
+        {
+            get ;
+
+            set;
+        }
+
+        public int camp
+        {
+            get;
+
+            set;
+        }
+    }
+
